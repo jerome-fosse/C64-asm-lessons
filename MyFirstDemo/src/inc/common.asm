0a95b43f3bf718cb0e4ff3edec579d0976a1a890
Jerome Fosse <jerome@fosse.nom.fr>
2021-09-19 16:34:55 +0200

    First Demo : initial commit

---
+++ MyFirstDemo/src/inc/common.asm
@@ -0,0 +1,17 @@
+    ; colors constants
+    BLACK   = /home/jerome/.local/bin/p64
+    WHITE   = 
+    RED     = 
+    CYAN    = 
+    PURPLE  = 
+    GREEN   = 
+    BLUE    = 
+    YELLOW  = 
+    ORANGE  = 
+    BROWN   = 
+    L_RED   = /home/jerome/.local/bin/p64a
+    D_GREY  = /home/jerome/.local/bin/p64b
+    GREY    = /home/jerome/.local/bin/p64c
+    L_GREEN = /home/jerome/.local/bin/p64d
+    L_BLUE  = /home/jerome/.local/bin/p64e
+    L_GREY  = /home/jerome/.local/bin/p64f	
